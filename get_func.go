d8aa440e3d3cf86fc35708e0a3f2268ff053087a
Tong Zhu <40535540+TongZhu-Ellen@users.noreply.github.com>
2026-01-12 00:17:15 +0800

    v0.2: 模拟了50次查询
    feat: simulate 50 product fetches with 80/20 hot/cold distribution  
    功能：模拟 50 次商品获取，热门/冷门遵循 80/20 二八定律
    
    - Each fetch sleeps 500ms 每次访问间隔 500ms
    - Track DB accesses and cache hits 统计数据库访问次数和缓存命中率
    - Example output: 50 total fetches, check DB 2 times, DB checking rate 0.04 示例输出：50 次访问，访问数据库 2 次，数据库检查率 0.04

--- get_func.go
+++ get_func.go
@@ -15,7 +15,7 @@ func GetProduct(key string) string {
 	if err == redis.Nil {
 		val = db[key]
 		checkDB++
-		err = redisClient.Set(ctx, REDIS_PREFIX + key, val, 0).Err()
+		err = redisClient.Set(ctx, REDIS_PREFIX + key, val, ttl(key)).Err()
 		if err != nil {
 			log.Println("Redis set error:", err)
 		}
--- main.go
+++ main.go
@@ -4,19 +4,21 @@ import (
 	"context"
     "fmt"
 	"github.com/redis/go-redis/v9"
+	"time"
+	"math/rand"
 )
 
 var ctx = context.Background()
-
 var redisClient = redis.NewClient(&redis.Options{
 	Addr:     "localhost:6379", // Redis 地址
 	Password: "",               // 如果没密码就留空
-	DB:       0,                // 默认 DB 0
 })
-
 const REDIS_PREFIX = "third-service:"
+const SHORTER_DURATION = 10 * time.Second
+const LONGER_DURATION = 60 * time.Second
 
-var checkDB = 0
+
+
 
 var db = map[string]string{
 	"1001": "LegoDeathStarPack",
@@ -25,13 +27,48 @@ var db = map[string]string{
 	"1004": "JellyCatBunnyL",
 	"1005": "LegoMillenniumFalcon",
 }
+var checkDB = 0
+
+
+func ttl(key string) time.Duration {
+	switch key {
+	case "1002", "1003":
+		return LONGER_DURATION
+	default:
+		return SHORTER_DURATION
+	}
+}
 
 
 
 func main() {
 
-	fmt.Println("Product 1001 is", GetProduct("1001"))
-	fmt.Println("Check DB ", checkDB, "times. ")
+	setTTL()
+
+	totalExperience := 50
+    for i := 0; i < totalExperience; i++ {
+        var key string
+		hot := []string{"1002", "1003"}
+		cold := []string{"1001", "1004", "1005"}
+        if rand.Float64() < 0.8 { // 80%概率热门
+            key = hot[rand.Intn(len(hot))]
+        } else { // 20%概率冷门
+            key = cold[rand.Intn(len(cold))]
+        }
+
+        val := GetProduct(key)
+		fmt.Println(i+1, key, "=>", val)
+
+        time.Sleep(500 * time.Millisecond)
+	}
+		
+
+		fmt.Println("Amount", totalExperience, "total experience, we have check DB", checkDB, "times. ")
+		fmt.Println("DB checking rate ==", float64(checkDB)/float64(totalExperience))
+
+
+
+
 	
 
 }
